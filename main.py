a85f63285d1e95b7f5c6fbc9284448342bd5b7e3
Yotam Korah <yotamkorah@gmail.com>
2020-11-21 23:28:36 +0200

    debugging

--- main.py
+++ main.py
@@ -3,16 +3,20 @@
 import time
 import re
 import analize
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from pathlib import Path
 
 ID = "324272202"  # Put your ID number here
+
+path = Path(__file__).resolve().parent
 
 mobile_emulation = {"deviceName": "Nexus 5"}
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(os.path.join(path, r"chromedriver.exe"), options=chrome_options)
 driver.get("https://clearance.medical.idf.il/")
 
 id_in = driver.find_element(By.XPATH, "/html/body/div/div/div/div/div[1]/div[4]/div[1]/div/div/input")
@@ -33,16 +37,21 @@ buttons = [
 for button in buttons:
     button.click()
 
-image_path = "numbers.png"
+image_path = os.path.join(path, "numbers.png")
 driver.save_screenshot(image_path)
 text = analize.run(image_path)
 
 digits = re.findall(r"\d\d\d\d", text)
 selected = digits[-1]
+if len(selected) == 5:
+    selected = selected[1::]
 
 for i in range(4):
     num = driver.find_element(By.XPATH, f"/html/body/div/div/div/div/div/div[4]/div/input[{i+1}]")
     num.send_keys(selected[i])
+
+driver.close()
+os.system(fr"date /T >> {path}\autoCorona.log")
 
 """
 import pywhatkit
---
+++ runAutoCorona.bat
@@ -0,0 +1,2 @@
+cd C:/Users/yotam/Documents/Python/AutoCorona/
+C:\Users\yotam\AppData\Local\Programs\Python\Python38-32\python.exe C:/Users/yotam/Documents/Python/AutoCorona/main.py >> C:/Users/yotam/Documents/Python/test.log
